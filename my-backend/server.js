d6297ebe796a2db5fce2a4aff01cd45f3697669c
something-man <minnalkodi735@gmail.com>
2024-12-22 17:59:55 +0530

    five

--- my-backend/server.js
+++ my-backend/server.js
@@ -1,3 +1,45 @@
+// const express = require('express');
+// const connectDB = require('./db');
+// const cors = require('cors');
+// const productRoutes = require('./routes/productRoutes');
+// const paymentRoutes = require('./routes/paymentRoutes');
+// require('dotenv').config();
+// const path = require('path');
+
+// const app = express();
+
+// // Connect to database
+// connectDB();
+
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+//   methods: ['GET', 'POST', 'OPTIONS'],
+//   credentials: true, // Allow credentials (cookies, authorization headers)
+// }));
+
+// app.use(express.json()); // Parse JSON bodies
+
+// // Use routes
+// app.use('/api', productRoutes);
+// app.use('/api', paymentRoutes);
+// // Start the server
+// app.use(express.static(path.join(__dirname, '../build')));
+
+// // Route to serve index.html for all non-API requests
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../build', 'index.html'));
+// });
+
+
+// const PORT = process.env.PORT || 5000;
+
+// const HOST = '0.0.0.0'; // Bind to all network interfaces
+
+// app.listen(PORT, HOST, () => {
+//   console.log(`Server running on http://${HOST}:${PORT}`);
+// });
+
+
 const express = require('express');
 const connectDB = require('./db');
 const cors = require('cors');
@@ -5,40 +47,41 @@ const productRoutes = require('./routes/productRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 require('dotenv').config();
 const path = require('path');
-
 const app = express();
 
 // Connect to database
-connectDB();
+connectDB().catch((error) => {
+    console.error('Error connecting to database:', error);
+    process.exit(1);
+});
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST', 'OPTIONS'],
-  credentials: true, // Allow credentials (cookies, authorization headers)
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true,
 }));
 
-app.use(express.json()); // Parse JSON bodies
+app.use(express.json());
 
 // Use routes
 app.use('/api', productRoutes);
 app.use('/api', paymentRoutes);
+
 // Start the server
 app.use(express.static(path.join(__dirname, '../build')));
 
 // Route to serve index.html for all non-API requests
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
-
 
 const PORT = process.env.PORT || 5000;
-
-const HOST = '0.0.0.0'; // Bind to all network interfaces
+const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+    console.log(`Server running on http://${HOST}:${PORT}`);
+}).on('error', (error) => {
+    console.error('Error starting server:', error);
+    process.exit(1);
 });
-
-
-
 
